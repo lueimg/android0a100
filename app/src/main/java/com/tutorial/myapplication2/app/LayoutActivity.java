d7af4201e126369af74d6bf36157dd40d687b0fc
luis mori <luismorith@gmail.com>
2014-06-27 13:24:46 -0500

    Uso de linear layout

---
+++ app/src/main/java/com/tutorial/myapplication2/app/LayoutActivity.java
@@ -0,0 +1,19 @@
+package com.tutorial.myapplication2.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by luismori on 27/06/14.
+ */
+public class LayoutActivity extends Activity {
+
+    @Override
+    public void onCreate( Bundle savedInstance ){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.linear_layout);
+
+
+
+    }
+}
