528e1dfdce288b19a4634086ca7a4b15b21f4841
luis mori <luismorith@gmail.com>
2014-06-27 13:45:17 -0500

    creando un login con table layout

--- app/src/main/java/com/tutorial/myapplication2/app/LayoutActivity.java
+++ app/src/main/java/com/tutorial/myapplication2/app/LayoutActivity.java
@@ -11,7 +11,7 @@ public class LayoutActivity extends Activity {
     @Override
     public void onCreate( Bundle savedInstance ){
         super.onCreate(savedInstance);
-        setContentView(R.layout.linear_layout);
+        setContentView(R.layout.table_layout);
 
 
 
